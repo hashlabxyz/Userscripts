c771b827ca41969e66b4e6624688d19b7a949495
Hash G <hf.hashg@gmail.com>
2016-04-28 17:47:42 +0200

    Clickable awards: v0.1.1
    Links open in a new tab
    Added YTB & Instagrammy awards

--- HackForums/Clickable_Sticky_Man.user.js
+++ HackForums/Clickable_Sticky_Man.user.js
@@ -4,14 +4,15 @@
 // @description http://hackforums.net/showthread.php?tid=5252169
 // @include     http://hackforums.net/showthread.php?tid=*
 // @require     http://code.jquery.com/jquery-2.2.2.min.js
-// @version     0.1.0
+// @version     0.1.1
 // @grant       GM_getValue
 // ==/UserScript==
 
-if ($("img[src='/uploads/awards/sticky-bro.png']").length > 0) {
+
+if ($("img[src='/uploads/awards/sticky-bro.png'], img[src='/uploads/awards/instagrammy.png'], img[src='/uploads/awards/YouTube.png']").length > 0) {
     var url;
-    $("img[src='/uploads/awards/sticky-bro.png']").each(function() {
+    $("img[src='/uploads/awards/sticky-bro.png'], img[src='/uploads/awards/instagrammy.png'], img[src='/uploads/awards/YouTube.png']").each(function() {
         url = $(this).attr("title");
-        $(this).wrap("<a href='" + url + "'></a>")
+        $(this).wrap("<a href='" + url + "' target='_blank'></a>")
     });
-}
+} 
