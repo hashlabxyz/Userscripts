e1cc6e5326fec634f37bf4cc9773103e3300ec7d
Hash G <hf.hashg@gmail.com>
2016-06-03 10:43:23 +0200

    Added clovers awards

--- HackForums/Clickable_Sticky_Man.user.js
+++ HackForums/Clickable_Sticky_Man.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name        Clickable Sticky Man
+// @name        Clickable Sticky Man, YouTube, Clovers awards
 // @namespace   Hash G.
 // @description http://hackforums.net/showthread.php?tid=5252169
 // @include     http://hackforums.net/showthread.php?tid=*
 // @require     http://code.jquery.com/jquery-2.2.2.min.js
-// @version     0.1.1
+// @version     0.1.2
 // @grant       GM_getValue
 // ==/UserScript==
 
@@ -15,4 +15,13 @@ if ($("img[src='/uploads/awards/sticky-bro.png'], img[src='/uploads/awards/insta
         url = $(this).attr("title");
         $(this).wrap("<a href='" + url + "' target='_blank'></a>")
     });
-} 
+}
+
+if ($("img[src*='shamrock.png']").length > 0) {
+    var url;
+    $("img[src*='shamrock.png']").each(function() {
+        var tid = $(this).attr("title").replace(/.*thread ([0-9]+).$/ig, "$1");
+        url = "http://hackforums.net/showthread.php?tid=" + tid;
+        $(this).wrap("<a href='" + url + "' target='_blank'></a>");
+    });
+}
