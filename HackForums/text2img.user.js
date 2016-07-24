02de19d40a4d0f14262ecea11e38e03fba014474
Hash G <hf.hashg@gmail.com>
2016-07-25 01:15:04 +0200

    text2img update for specific forum

--- HackForums/text2img.user.js
+++ HackForums/text2img.user.js
@@ -2,15 +2,16 @@
 // @name        text2img
 // @namespace   Hash G.
 // @author      Hash G.
-// @include     http://hackforums.net/showthread.php?tid=*
+// @include     http://www.ac-web.org/forums/showthread.php?*
+// @include     http://www.ac-web.org/forums/newreply.php?do=postreply&t=*
 // @require     http://code.jquery.com/jquery-2.2.2.min.js
-// @version     0.1.0
+// @version     0.1.1
 // @grant       GM_getValue
 // ==/UserScript==
 
 
-$("#message").on("keyup", function(e) {
-    var message = $(this).val();
+$("body").on("keyup", ".cke_source", function(e) {
+    var message = $(".cke_source").val();
     var search = /(\[img\])*http(.*?)(png|jpg|jpeg|gif)/ig;
 
     var arr = message.match(search);
@@ -19,7 +20,7 @@ $("#message").on("keyup", function(e) {
         for (var i = 0; i < arr.length; i++) {
             if (arr[i].substr(0, 5) != "[img]") {
                 message = message.replace(arr[i], "[img]" + arr[i] + "[/img]");
-                $("#message").val(message);
+                $(".cke_source").val(message);
             }
         }
     }
