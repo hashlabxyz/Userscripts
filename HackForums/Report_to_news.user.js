601e3119ba90251bd2f22e73a9e4f043ac3900f9
Hash G <hf.hashg@gmail.com>
2016-12-19 23:47:02 +0100

    Update Report_to_news.user.js

--- HackForums/Report_to_news.user.js
+++ HackForums/Report_to_news.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name        Report to news
 // @description Allow you to make a quick report to the news by pressing the "News report" link at the top-right.
-// @namespace   Hash G.
+// @namespace   HF
+// @author      Hash G.
 // @include     *hackforums.net*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
-// @version     1.03
+// @version     1.04
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
@@ -42,7 +43,7 @@ $(document).ready(function() {
 	}
 	
 
-	$("body").append("<div id='popup_news' style='background-color: rgb(51, 51, 51); bottom: auto; border: 1px solid rgb(0, 0, 0); height: 30%; left: 182px; margin: 0px; max-height: 95%; max-width: 95%; opacity: 1; overflow: auto; padding: 0px; position: fixed; right: auto; top: 128px; width: 75%; z-index: 999; display: none;'><span style='float: right; margin-right: 1%; margin-top: 0.5%;'><a href='http://hackforums.net/showthread.php?tid=4992602&action=lastpost' target='_blank'>News thread</a></span><br><h4>Briefly describe the event: </h4><input id='step1' style='width: 70%' type='text' value='"+event+"'><br><h4>Any important links: </h4><input id='step2' style='width: 70%' type='text' value="+link+"><br><br><br><button class='bitButton' id='sendNews' onclick='sendNews()'>Send!</button> <button class='bitButton' id='closeNews'>Close</button><br><br><span id='status'></span></div>");
+	$("body").append("<div id='popup_news' style='background-color: rgb(51, 51, 51); bottom: auto; border: 1px solid rgb(0, 0, 0); height: 30%; left: 182px; margin: 0px; max-height: 95%; max-width: 95%; opacity: 1; overflow: auto; padding: 0px; position: fixed; right: auto; top: 128px; width: 75%; z-index: 999; display: none;'><span style='float: right; margin-right: 1%; margin-top: 0.5%;'><a href='https://hackforums.net/showthread.php?tid=5470739&action=lastpost' target='_blank'>News thread</a></span><br><h4>Briefly describe the event: </h4><input id='step1' style='width: 70%' type='text' value='"+event+"'><br><h4>Any important links: </h4><input id='step2' style='width: 70%' type='text' value="+link+"><br><br><br><button class='bitButton' id='sendNews' onclick='sendNews()'>Send!</button> <button class='bitButton' id='closeNews'>Close</button><br><br><span id='status'></span></div>");
 	$(".links").append(" | <a href='#' id='showNews'>News report</a>");
 
 	$("#sendNews").on("click", function() {
@@ -53,7 +54,7 @@ $(document).ready(function() {
 		$.post("http://hackforums.net/newreply.php", {
 			"my_post_key": postkey,
 			"action": "do_newreply",
-			"tid": "4992602",
+			"tid": "5470739",
 			"message": report,
 			"modoptions[stickthread]" : 1
 		},
